e639ce4b039b871be597ccec30579584fe5de18b
Eddy518 <edmwangi2@gmail.com>
2024-05-19 08:51:43 +0300

    Fix reset button not working

--- index.js
+++
@@ -1,7 +0,0 @@
-const reset = document.querySelector("#reset");
-const formInput = document.querySelectorAll("input");
-
-reset.addEventListener("click", (e) => {
-  e.preventDefault();
-  formInput.forEach((input) => (input.value = ""));
-});
