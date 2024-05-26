7876a67a949aa3f9561195c5bd9d975f6b2a0e7b
Eddy518 <edmwangi2@gmail.com>
2024-05-26 10:43:35 +0300

    This closes #3

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const copyrightYear = document.querySelector(".copyright-year");
 console.log(copyrightYear);
 copyrightYear.textContent = new Date().getFullYear();
+
+const filterBtn = document.querySelector("#tbl");
+
+document.addEventListener("DOMContentLoaded", () => (filterBtn.value = ""));
