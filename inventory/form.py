aeca9dbc93eee50907ee4b548817f2be4dd18ba7
Alex Sheunda <sheundaalex@gmail.com>
2024-06-22 18:19:43 +0300

    Friday changes

--- inventory/form.py
+++ inventory/form.py
@@ -47,4 +47,4 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit = SubmitField('Login')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
